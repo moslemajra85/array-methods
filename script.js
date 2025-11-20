b0957c4b92e3b2623446d97c1d0167f8a4dcc6b6
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2025-11-20 17:29:21 +0100

    more methods

--- script.js
+++ script.js
@@ -27,4 +27,104 @@ console.log("Updated Array:", numbers);
 
 // remove from the tail(end)
 
+numbers.pop();
+
+console.log("Updated Array:", numbers);
+
 // remove from the head(beginning)
+
+function clearArray(array) {
+  //array.length = 0;
+}
+
+test = [1, 2, 3, 4, 5];
+clearArray(test);
+console.log(test);
+
+const names = ["Adam", "Anna", "Bob"];
+
+console.log("Original Names: ", names);
+
+names.splice(1, 1);
+
+console.log("Updated Names: ", names);
+
+/**
+ *
+ * push: add to the end of the array
+ * unshift: add to the  beginning
+ * pop : remove from the ned
+ * splice: remove from anywhere
+ * reverse: reverse  to the opposite order
+ * indexOf: return the index if element is found otherwise -1
+ * lastIndexOf: return the index of the last ouccrence
+ * forEach: give access to each element in the array with each iteration
+ * Join:
+ * includes
+ *
+ */
+
+let notes = [10, 20, 300, 40, 50, 30];
+
+//notes.reverse();
+
+console.log("index: ", notes.indexOf(30));
+console.log("index: ", notes.lastIndexOf(30));
+
+console.log(
+  "###########################################################################"
+);
+
+function max(array) {
+  let sup = array[0];
+
+  for (let e of array) {
+    if (e > sup) {
+      sup = e;
+    }
+  }
+
+  return sup;
+}
+
+function maxValue(array) {
+  let max = array[0];
+
+  array.forEach(function (e) {
+    if (e > max) {
+      max = e;
+    }
+  });
+
+  return max;
+}
+
+function displayArray(array) {
+  array.forEach(function (e) {
+    console.log(e);
+  });
+}
+
+let result = max(notes);
+console.log(result);
+result = maxValue(notes);
+console.log(result);
+
+// notes.forEach(function (a, b) {
+//   console.log(a, b);
+// });
+
+console.log("####Show Array Elements Using For Each####");
+displayArray(notes);
+console.log("####Show Array Elements Using For Each####");
+
+const words = ["Hello", "From", "Javascript!"];
+
+console.log(words);
+
+console.log(words.join("..."));
+
+console.log(words);
+
+const rs = words.includes("Hello123456");
+console.log(rs);
